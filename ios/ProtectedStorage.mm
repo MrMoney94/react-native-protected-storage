639f8be58606174c9a11f1eeada65120284cd9f6
Sebastian <sebas_fvb09@hotmail.com>
2024-01-04 11:19:05 -0500

    feat: corrección de error en la carga de datos

--- ios/ProtectedStorage.mm
+++ ios/ProtectedStorage.mm
@@ -2,7 +2,30 @@
 
 @interface RCT_EXTERN_MODULE(ProtectedStorage, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+RCT_EXTERN_METHOD(setItem:(NSString)name withValue:(NSString)value
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getItem:(NSString)name
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(removeItem:(NSString)name
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(clearAll: (RCTPromiseResolveBlock)resolve 
+                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(storageSetItem:(NSString)name withValue:(NSString)value
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(storageGetItem:(NSString)name
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(storageRemoveItem:(NSString)name
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
--- ios/ProtectedStorage.swift
+++ ios/ProtectedStorage.swift
@@ -1,8 +1,126 @@
+import Security
+
 @objc(ProtectedStorage)
 class ProtectedStorage: NSObject {
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
+  @objc(setItem:withValue:withResolver:withRejecter:)
+  func setItem(name: String, value: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+    let key = name
+    let password = value.data(using: .utf8)!
+    let attributes: [String: Any] = [
+      kSecClass as String: kSecClassGenericPassword,
+      kSecAttrAccount as String: key,
+      kSecValueData as String: password,
+    ]
+
+    let updateAttributes: [String: Any] = [
+      kSecValueData as String: password,
+    ]
+
+    let query: [String: Any] = [
+      kSecClass as String: kSecClassGenericPassword,
+      kSecAttrAccount as String: key,
+    ]
+
+    let status = SecItemUpdate(query as CFDictionary, updateAttributes as CFDictionary)
+
+    if status == errSecSuccess {
+      resolve("Item successfully updated.")
+    } else if status == errSecItemNotFound {
+      let result = SecItemAdd(attributes as CFDictionary, nil)
+      if result == errSecSuccess {
+        resolve("Item successfully added.")
+      } else {
+        resolve(nil)
+      }
+    } else {
+      resolve(nil)
+    }
+  }
+
+  @objc(getItem:withResolver:withRejecter:)
+  func getItem(name: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+    let key = name
+    let query: [String: Any] = [
+      kSecClass as String: kSecClassGenericPassword,
+      kSecAttrAccount as String: key,
+      kSecReturnData as String: true,
+    ]
+
+    var result: AnyObject?
+    let status = SecItemCopyMatching(query as CFDictionary, &result)
+
+    if status == errSecSuccess, let data = result as? Data {
+      if let password = String(data: data, encoding: .utf8) {
+        resolve(password)
+      } else {
+        reject("KeychainError", "Failed to decode data from keychain.", nil)
+      }
+    } else if status == errSecItemNotFound {
+      resolve(nil)
+    } else {
+      reject("KeychainError", "Failed to retrieve item from keychain.", nil)
+    }
+  }
+
+  @objc(removeItem:withResolver:withRejecter:)
+  func removeItem(name: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+    let key = name
+    let query: [String: Any] = [
+      kSecClass as String: kSecClassGenericPassword,
+      kSecAttrAccount as String: key,
+    ]
+
+    let status = SecItemDelete(query as CFDictionary)
+
+    if status == errSecSuccess {
+      resolve("Item successfully removed.")
+    } else if status == errSecItemNotFound {
+      resolve(nil)
+    } else {
+      reject("KeychainError", "Failed to remove item from keychain.", nil)
+    }
+  }
+
+  @objc(clearAll:withRejecter:)
+  func clearAll(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+    let query: [String: Any] = [
+      kSecClass as String: kSecClassGenericPassword,
+    ]
+
+    let status = SecItemDelete(query as CFDictionary)
+
+    if status == errSecSuccess || status == errSecItemNotFound {
+      let defaults = UserDefaults.standard
+      if let bundleID = Bundle.main.bundleIdentifier {
+          defaults.removePersistentDomain(forName: bundleID)
+      }
+      resolve("All items successfully removed.")
+    } else {
+      resolve(nil)
+    }
+  }
+
+  @objc(storageSetItem:withValue:withResolver:withRejecter:)
+  func storageSetItem(name: String, value: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
+    UserDefaults.standard.set(value, forKey: name)
+    resolve("Save in storage")
+  }
+
+  @objc(storageGetItem:withResolver:withRejecter:)
+  func storageGetItem(name: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+    let userDefaults = UserDefaults.standard
+
+    if let stringValue = userDefaults.string(forKey: name) {
+        resolve(stringValue)
+    } else {
+        resolve(nil)
+    }
+  }
+
+  @objc(storageRemoveItem:withResolver:withRejecter:)
+  func storageRemoveItem(name: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+    UserDefaults.standard.removeObject(forKey: name)
+    resolve(nil)
   }
 }
